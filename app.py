ab81286a42807bc982261b412c19a13f7a8a2612
adityagupta-dev07 <aditya.phantom07@gmail.com>
2025-12-29 21:09:45 +0530

    Format and clean up app.py code

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def gen_frames():
             b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
         )
 
-@app.route("/")
+@app.route("0/https://github.com/adityagupta-dev07/FACE_EXPRESSION_DETECTION/blob/main/templates/index.html")
 def index():
     meme = get_meme_by_expression(current_expression)
     return render_template(
@@ -72,3 +72,4 @@ def meme():
 
 if __name__ == "__main__":
     app.run(debug=False, threaded=True)
+
